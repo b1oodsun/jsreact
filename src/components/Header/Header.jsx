fd3e3304d4333fa1914bc73d3033cbb9a29e9f01
b1oodsun <148546113+b1oodsun@users.noreply.github.com>
2023-11-21 16:10:47 +0600

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,19 @@ import './style.css'
 function Header (){
     return(
         <header>
-            <a href="#"><img className="button-img"  src="./img/google-bard-icon.svg" alt="logo" /></a>
+            <span lassName="title-span"c>
+            <a className="button-img" href="#">
+                <img src="https://www.pinnacle.com/Cms_Data/Contents/Guest/Media/esports2017/Article-Images/Valorant/2020-What-is-Valorant/Esports-What-is-Valorant-Temp-Social-Hero.jpg" alt="logo"/></a>
+            <span>
             <a className="button" href="#">clickme</a>
             <a className="button" href="#">clickme</a>
             <a className="button" href="#">clickme</a>
             <a className="button" href="#">clickme</a>
-            <input type="text" placeholder="title-text" />
+            <input type="text" placeholder="title-text"/>
+            </span>
+
+            </span>
         </header>
-);
+)
 }
 export default Header;
